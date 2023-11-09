60b8c65651ea413867b8ea0caedfed50fb68c239
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-11-09 09:25:36 -0300

    Acessos de acordo com roles ok

--- src/main/java/com/uniamerica/springsecuritytest/controller/UsuarioController.java
+++ src/main/java/com/uniamerica/springsecuritytest/controller/UsuarioController.java
@@ -3,7 +3,12 @@ package com.uniamerica.springsecuritytest.controller;
 import com.uniamerica.springsecuritytest.entity.Usuario;
 import com.uniamerica.springsecuritytest.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,22 +18,30 @@ public class UsuarioController {
     @Autowired
     private UsuarioService service;
 
-    @GetMapping
-    public String userPath(){
-        return "<h1>USER</h1>";
-    }
     @GetMapping("/admin")
-    public String admPath(){
-        return "<h1>ADMIN</h1>";
-    }
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<String> admPath(){
+            return ResponseEntity.ok("Acesso concedido apenas para ADMIN");
+        }
     @GetMapping("/cliente")
-    @PreAuthorize("hasRole('CLIENTE')")
-    public String clientePath(){
-        return "<h1>CLIENTE</h1>";
+    @PreAuthorize("hasAnyAuthority('CLIENTE', 'ADMIN')")
+    public ResponseEntity<String> clientePath(){
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Verifica se o usuário possui a função (role) desejada
+//        if (authentication != null && authentication.getAuthorities().stream()
+//                .anyMatch(authority -> "CLIENTE".equals(authority.getAuthority()))) {
+            // O usuário tem a função (role) "CLIENTE"
+            return ResponseEntity.ok("Acesso concedido apenas para CLIENTE");
+//        } else {
+//            // O usuário não tem permissão
+//            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+//        }
     }
 
 
     @PostMapping("/post")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String create(@RequestBody Usuario usuario){
         try{
             return service.Create(usuario);
--- src/main/java/com/uniamerica/springsecuritytest/entity/Usuario.java
+++ src/main/java/com/uniamerica/springsecuritytest/entity/Usuario.java
@@ -6,9 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor
@@ -32,7 +35,11 @@ public class Usuario implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        Set<SimpleGrantedAuthority> authoritySet = new HashSet<>();
+
+        authoritySet.add(new SimpleGrantedAuthority(role));
+
+        return authoritySet;
     }
 
     @Override
